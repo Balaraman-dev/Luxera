33ffe7c984e908f6d3edf17c85aa3996710174c2
Balaraman-dev <balaramann2004@gmail.com>
2024-11-30 21:34:46 +0530

    correction

--- src/components/Landing_page/Landing_view.jsx
+++ src/components/Landing_page/Landing_view.jsx
@@ -5,7 +5,7 @@ import Card_slider from './Card_slider_new.jsx'
 import Recent_list from './Recent_list.jsx'
 import View_all from './View_all.jsx'
 import searchimg from ".\src\assets\search.png";
-import dotting from "src/assets/dotted_top.png";
+import dotting from "./src/assets/dotted_top.png";
 
 const Landing_view = ({recent}) => {
    const[apilist,setApilist]=useState(0);
